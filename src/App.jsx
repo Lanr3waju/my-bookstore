24985735574ea57383dc8df4f3f5a001e01e67cc
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-01-16 10:05:24 +0100

    Create route for categories component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Book from "./components/Book";
 import Books from "./components/Books";
+import Categories from "./components/Categories";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <h1>My BookStore</h1>
       <Routes>
         <Route path="/book/:id" element={<Book />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/" element={<Books />} />
       </Routes>
     </BrowserRouter>
