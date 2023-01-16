59ba644c279035828cbeea0c9ac7697352470067
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-01-16 09:46:42 +0100

    Create Categories component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,13 +1,17 @@
+import AddBook from "./AddBook";
 import Book from "./Book";
 
 function Books() {
     return (
-        <ul>
-            <li>
-                <Book />
-                <button type="button">Remove</button>
-            </li>
-        </ul>
+        <>
+            <ul>
+                <li>
+                    <Book />
+                    <button type="button">Remove</button>
+                </li>
+            </ul>
+            <AddBook />
+        </>
     )
 }
 
