8219862538d54513276e43da0e8a72b747c49f5a
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-01-18 08:32:45 +0100

    Refactor Books component

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,14 +1,32 @@
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import AddBook from "./AddBook";
 import Book from "./Book";
 
 function Books() {
+  const [books, setBooks] = useState([])
+
+  const myBook = [
+    { author: "Kyle Simpson", title: "You no Sabi JavaScript" },
+    { author: "Chinua Achebe", title: "Things Fall Apart" }
+  ]
+
+  useEffect(() => {
+    setBooks(myBook)
+  }, [1]);
+
+  const bookEl = books.map((book) => (
+    <li key={book.title}>
+      <Link to={`/book/${book.title}`}>
+        <Book author={book.author} title={book.title} />
+      </Link>
+    </li>
+  )) || ""
+
   return (
     <>
       <ul>
-        <li>
-          <Book />
-          <button type="button">Remove</button>
-        </li>
+        {bookEl}
       </ul>
       <AddBook />
     </>
