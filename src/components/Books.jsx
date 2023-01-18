3f0dc1b5a877b513bea2faf03a4bbd3b081e0422
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-01-18 08:40:02 +0100

    style: Format with prettier

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,33 +1,24 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import AddBook from "./AddBook";
 import Book from "./Book";
 
+
 function Books() {
-  const [books, setBooks] = useState([])
+  const [books] = useState([]);
 
-  const myBook = [
-    { author: "Kyle Simpson", title: "You no Sabi JavaScript" },
-    { author: "Chinua Achebe", title: "Things Fall Apart" }
-  ]
-
-  useEffect(() => {
-    setBooks(myBook)
-  }, [1]);
-
-  const bookEl = books.map((book) => (
-    <li key={book.title}>
-      <Link to={`/book/${book.title}`}>
-        <Book author={book.author} title={book.title} />
-      </Link>
-    </li>
-  )) || ""
+  const bookEl =
+    books.map((book) => (
+      <li key={book.title}>
+        <Link to={`/book/${book.title}`} key={book.title}>
+          <Book key={book.title} author={book.author} title={book.title} />
+        </Link>
+      </li>
+    )) || "";
 
   return (
     <>
-      <ul>
-        {bookEl}
-      </ul>
+      <ul>{bookEl}</ul>
       <AddBook />
     </>
   );
