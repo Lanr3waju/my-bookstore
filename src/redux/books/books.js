2089419255758f565a6418146b747356abf95ca5
Abass Olanrewaju <abassolanrewaju111@gmail.com>
2023-02-14 21:22:51 +0100

    Remove double-quoted strings

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,28 +1,28 @@
-const ADD_BOOK = "bookStore/books/ADD_BOOK";
-const REMOVE_BOOK = "bookStore/books/REMOVE_BOOK";
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
 export const addBook = (payload) => ({
-  type: ADD_BOOK,
-  payload,
+    type: ADD_BOOK,
+    payload,
 });
 export const removeBook = (payload) => ({
-  type: REMOVE_BOOK,
-  payload,
+    type: REMOVE_BOOK,
+    payload,
 });
 
 const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_BOOK:
-      return [...state, action.payload];
+    switch (action.type) {
+        case ADD_BOOK:
+            return [...state, action.payload];
 
-    case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.payload);
+        case REMOVE_BOOK:
+            return state.filter((book) => book.id !== action.payload);
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 export default reducer;
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,14 +1,14 @@
 import {
-  legacy_createStore as createStore,
-  combineReducers,
-  applyMiddleware,
-} from "redux";
-import logger from "redux-logger";
-import booksReducer from "./books/books";
+    legacy_createStore as createStore,
+    combineReducers,
+    applyMiddleware,
+} from 'redux';
+import logger from 'redux-logger';
+import booksReducer from './books/books';
 
 const reducer = combineReducers({
-  booksReducer,
-  // additional reducers could be added here
+    booksReducer,
+    // additional reducers could be added here
 });
 
 const store = createStore(reducer, applyMiddleware(logger));
